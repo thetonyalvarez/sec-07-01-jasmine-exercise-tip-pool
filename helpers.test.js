4ad727cb9042cedd0629802735d7439ea04c84fe
Tony Alvarez <tonyalva87@gmail.com>
2022-02-06 21:24:39 -0800

    completed helpers.test.js

---
+++ helpers.test.js
@@ -0,0 +1,64 @@
+
+describe("Helpers test (with setup and tear-down)", () => {
+	beforeEach(function () {
+		// initialization logic
+		billAmtInput.value = 20;
+		tipAmtInput.value = 2;
+		submitPaymentInfo();
+	});
+
+	it("should calculate tip percent", () => {
+		expect(calculateTipPercent(100,10)).toEqual(10);
+		expect(calculateTipPercent(200,50)).toEqual(25);
+		expect(calculateTipPercent(60,10)).toEqual(17);
+	});
+
+	it("should sum all entries to billAmt and return total when using sumPaymentTotal()", () => {
+		expect(sumPaymentTotal('billAmt')).toEqual(20);
+		
+		billAmtInput.value = 60;
+		tipAmtInput.value = 10;
+		submitPaymentInfo();
+
+		expect(sumPaymentTotal('billAmt')).toEqual(80);
+	});
+
+	it("should sum all entries to tipAmt and return total when using sumPaymentTotal()", () => {
+		expect(sumPaymentTotal('tipAmt')).toEqual(2);
+		
+		billAmtInput.value = 60;
+		tipAmtInput.value = 10;
+		submitPaymentInfo();
+
+		expect(sumPaymentTotal('tipAmt')).toEqual(12);
+	});
+
+	it("should sum all tip percentages to tipPercent and return total when using sumPaymentTotal()", () => {
+		expect(sumPaymentTotal('tipPercent')).toEqual(10);
+		
+		billAmtInput.value = 60;
+		tipAmtInput.value = 10;
+		submitPaymentInfo();
+
+		expect(sumPaymentTotal('tipPercent')).toEqual(27);
+	});
+
+	it("should generate new td from value and append to tr on appendTd(tr, value)", function () {
+		let newTr = document.createElement('tr');
+
+		appendTd(newTr, 'test');
+		expect(newTr.children.length).toEqual(1);
+		expect(newTr.innerText).toEqual('test');
+		
+	});
+	afterEach(function() {
+		billAmtInput.value = '';
+		tipAmtInput.value = '';
+		allPayments = {};
+		paymentTbody.innerHTML = "";
+		summaryTds[0].innerHTML = "";
+		summaryTds[1].innerHTML = "";
+		summaryTds[2].innerHTML = "";
+		serverTbody.innerHTML = "";
+	});
+});
--- servers.test.js
+++ servers.test.js
@@ -11,7 +11,30 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+	it('should not add a new server on submitServerInfo() with empty input', function () {
+    serverNameInput.value = '';
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(0);
+  });
+
+	it('should update #servertable on updateServerTable()', function () {
+    submitServerInfo();
+    updateServerTable();
+
+    let serverTable = document.querySelectorAll('#serverTable tbody tr td');
+    expect(serverTable.length).toEqual(2);
+    expect(serverTable[0].innerText).toEqual('Alice');
+    expect(serverTable[1].innerText).toEqual('$0.00');
+
+  });
+
+
+
   afterEach(function() {
     serverNameInput.value = '';
+    serverId = 0;
+    serverTbody.innerHTML = ''
+    allServers = {}
   });
 });
